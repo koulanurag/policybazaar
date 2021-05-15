09e48f041ddc6778811856525ab343b19cf7727f
Anurag Koul <koulanurag@gmail.com>
2021-05-14 23:22:15 -0700

    feat: added cassie hop policy

--- policybazaar/config.py
+++ policybazaar/config.py
@@ -50,9 +50,11 @@ MUJOCO_ENV_IDS = {'HalfCheetah-v2': {'wandb_run_path': MUJOCO_BASE_PROJECT_URL +
 
 # Cassie
 CASSIE_BASE_PROJECT_URL = 'offline_drl_team/cassie/'
-CASSIE_ENV_IDS = {'cassie:CassieWalkSlow-v0': {'wandb_run_path': CASSIE_BASE_PROJECT_URL + '2l2ah4qu', 'models': {1: 0}},
-                  'cassie:CassieStand-v0': {'wandb_run_path': CASSIE_BASE_PROJECT_URL + '1016bat5', 'models': {1: 0}},
-                  'cassie:CassieWalkFast-v0': {'wandb_run_path': CASSIE_BASE_PROJECT_URL + '2mzeew14', 'models': {1: 0}}}
+CASSIE_ENV_IDS = {
+    'cassie:CassieWalkSlow-v0': {'wandb_run_path': CASSIE_BASE_PROJECT_URL + '2l2ah4qu', 'models': {1: 0}},
+    'cassie:CassieStand-v0': {'wandb_run_path': CASSIE_BASE_PROJECT_URL + '1016bat5', 'models': {1: 0}},
+    'cassie:CassieWalkFast-v0': {'wandb_run_path': CASSIE_BASE_PROJECT_URL + '2mzeew14', 'models': {1: 0}},
+    'cassie:CassieHop-v0': {'wandb_run_path': CASSIE_BASE_PROJECT_URL + '2u00mtk8', 'models': {1: 0}}}
 
 ENV_IDS = {**MAZE_ENV_IDS, **MUJOCO_ENV_IDS, **CASSIE_ENV_IDS}
 
